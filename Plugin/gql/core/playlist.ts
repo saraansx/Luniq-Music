143405d91a0d430e66ae2339e09611c434ad7dd6
saraansx <saraans.bali@icloud.com>
2026-07-02 02:45:15 +0530

    fix(spotify): fall back to REST API for playlist write operations on GQL failure

--- Plugin/gql/core/playlist.ts
+++ Plugin/gql/core/playlist.ts
@@ -63,75 +63,109 @@ class SpotifyPlaylistEndpoint {
     }
 
     async create(options: { name: string; description?: string; public?: boolean; collaborative?: boolean }) {
-        const hash = await getHash("Playlist", "createPlaylist");
+        try {
+            const hash = await getHash("Playlist", "createPlaylist");
 
-        const res = await this.gqlClient.post("query", {
-            body: {
-                variables: {
-                    name: options.name,
-                    description: options.description || "",
-                    public: options.public || false,
-                    collaborative: options.collaborative || false,
-                },
-                operationName: "createPlaylist",
-                extensions: {
-                    persistedQuery: {
-                        version: 1,
-                        sha256Hash: hash,
+            const res = await this.gqlClient.post("query", {
+                body: {
+                    variables: {
+                        name: options.name,
+                        description: options.description || "",
+                        public: options.public || false,
+                        collaborative: options.collaborative || false,
+                    },
+                    operationName: "createPlaylist",
+                    extensions: {
+                        persistedQuery: {
+                            version: 1,
+                            sha256Hash: hash,
+                        },
                     },
                 },
-            },
-        });
+            });
 
-        SpotifyError.mayThrow(res);
-        return res.data.createPlaylist;
+            SpotifyError.mayThrow(res);
+            return res.data.createPlaylist;
+        } catch (err) {
+            console.warn("[SpotifyPlaylistEndpoint] create GQL failed, falling back to REST API:", err);
+            const restRes = await this.gqlClient.post("https://api.spotify.com/v1/me/playlists", {
+                body: {
+                    name: options.name,
+                    description: options.description || "",
+                    public: options.public ?? false,
+                    collaborative: options.collaborative ?? false
+                }
+            });
+            return restRes;
+        }
     }
 
     async addTracks(playlistId: string, { uris, position }: { uris: string[]; position?: number }) {
-        const hash = await getHash("Playlist", "addToPlaylist");
+        try {
+            const hash = await getHash("Playlist", "addToPlaylist");
 
-        const res = await this.gqlClient.post("query", {
-            body: {
-                variables: {
-                    playlistUri: `spotify:playlist:${playlistId}`,
-                    uris,
-                    position: position ?? null,
-                },
-                operationName: "addItemsToPlaylist",
-                extensions: {
-                    persistedQuery: {
-                        version: 1,
-                        sha256Hash: hash,
+            const res = await this.gqlClient.post("query", {
+                body: {
+                    variables: {
+                        playlistUri: `spotify:playlist:${playlistId}`,
+                        uris,
+                        position: position ?? null,
+                    },
+                    operationName: "addItemsToPlaylist",
+                    extensions: {
+                        persistedQuery: {
+                            version: 1,
+                            sha256Hash: hash,
+                        },
                     },
                 },
-            },
-        });
+            });
 
-        SpotifyError.mayThrow(res);
-        return res;
+            SpotifyError.mayThrow(res);
+            return res;
+        } catch (err) {
+            console.warn("[SpotifyPlaylistEndpoint] addTracks GQL failed, falling back to REST API:", err);
+            const restRes = await this.gqlClient.post(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+                body: {
+                    uris,
+                    position: position ?? undefined
+                }
+            });
+            return restRes;
+        }
     }
 
     async removeTracks(playlistId: string, { uris }: { uris: string[] }) {
-        const hash = await getHash("Playlist", "removeFromPlaylist");
+        try {
+            const hash = await getHash("Playlist", "removeFromPlaylist");
 
-        const res = await this.gqlClient.post("query", {
-            body: {
-                variables: {
-                    playlistUri: `spotify:playlist:${playlistId}`,
-                    uris,
-                },
-                operationName: "removeItemsFromPlaylist",
-                extensions: {
-                    persistedQuery: {
-                        version: 1,
-                        sha256Hash: hash,
+            const res = await this.gqlClient.post("query", {
+                body: {
+                    variables: {
+                        playlistUri: `spotify:playlist:${playlistId}`,
+                        uris,
+                    },
+                    operationName: "removeItemsFromPlaylist",
+                    extensions: {
+                        persistedQuery: {
+                            version: 1,
+                            sha256Hash: hash,
+                        },
                     },
                 },
-            },
-        });
+            });
 
-        SpotifyError.mayThrow(res);
-        return res;
+            SpotifyError.mayThrow(res);
+            return res;
+        } catch (err) {
+            console.warn("[SpotifyPlaylistEndpoint] removeTracks GQL failed, falling back to REST API:", err);
+            const restRes = await this.gqlClient.delete(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+                body: {
+                    tracks: uris.map(uri => ({ uri }))
+                }
+            });
+            return restRes;
+        }
     }
 
     async follow(playlistIds: string[]) {
